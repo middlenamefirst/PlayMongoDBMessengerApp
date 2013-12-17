41742032352c4cf33c75fb4d86c33eb2c8dfbe63
Marc Barry <mdbarry@gmail.com>
2013-12-17 12:06:28 -0500

    Removed default (and broken) tests.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -18,13 +18,5 @@ class ApplicationSpec extends Specification {
     "send 404 on a bad request" in new WithApplication{
       route(FakeRequest(GET, "/boum")) must beNone
     }
-
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
-    }
   }
 }
